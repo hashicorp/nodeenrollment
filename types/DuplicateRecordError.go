7ee5ab3f4390b048d3a68b99485da8cd2a3b139c
irenarindos <irena.rindos@gmail.com>
2023-07-12 12:05:39 -0400

    fixup! Add DuplicateRecordError and handing to authorizeNodeCommon

--- types/DuplicateRecordError.go
+++ types/DuplicateRecordError.go
@@ -3,26 +3,11 @@
 
 package types
 
-import "fmt"
-
 const dupeRecordErr = "duplicate NodeInformation record found"
 
 type DuplicateRecordError struct {
-	NodeInfo *NodeInformation
 }
 
-func NewDuplicateRecordError(n *NodeInformation) (DuplicateRecordError, error) {
-	const op = "nodeenrollment.types.NewDuplicateRecordError"
-	if n == nil {
-		return DuplicateRecordError{}, fmt.Errorf("(%s) nil node information", op)
-	}
-	return DuplicateRecordError{NodeInfo: n}, nil
-}
-
-func (d *DuplicateRecordError) Error() string {
+func (d DuplicateRecordError) Error() string {
 	return dupeRecordErr
-}
-
-func (d *DuplicateRecordError) GetNodeInformation() *NodeInformation {
-	return d.NodeInfo
 }
